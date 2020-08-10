bc3788af8f4399e525b7538301ac62ef40a47bc1
Matthew Blank <msb277@cornell.edu>
2020-08-10 23:15:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,22 @@ class Dog
     @breed = breed
   end
 
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
   end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+
 end
